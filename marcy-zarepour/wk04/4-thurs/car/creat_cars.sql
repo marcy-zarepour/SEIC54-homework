bd3b2d4fa52bf5f6649210b2cd23e89466949e03
Marcy <marcy@Marcys-MBP.lan>
2022-08-23 20:13:25 +1000

    WK04-thurs

---
+++ marcy-zarepour/wk04/4-thurs/car/creat_cars.sql
@@ -0,0 +1,11 @@
+CREATE TABLE cars(
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    model TEXT,
+    image TEXT,
+    
+);
+
+INSERT INTO cars(name , model) VALUES ('Toyota','camery');
+INSERT INTO cars(name , model) VALUES ('Tesla','y');
+INSERT INTO cars(name , model) VALUES ('BMW' , 'I6I8');
